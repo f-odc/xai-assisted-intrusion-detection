b635a5accdec6df76bbf111a23136ed58c091c93
f-odc <f.o.d.c@web.de>
2025-02-26 13:59:12 +0100

    Add conversion function

--- src/functions/explainer.py
+++ src/functions/explainer.py
@@ -4,6 +4,7 @@ explainer: This module contains functions for generating explanations for ML mod
 Functions:
 - generate_shap_explainer: Generates a SHAP explainer for a model.
 - generate_shap_values: Generates SHAP values for a model.
+- convert_shap_values_to_pd: Converts SHAP values to a pandas DataFrame.
 - plot_shap_summary: Plots a SHAP summary plot.
 
 Usage:
@@ -11,8 +12,6 @@ Usage:
 >>> import explainer as exp
 >>> shap_explainer = exp.generate_shap_explainer(model, mask)
 """
-
-import numpy as np
 
 import pandas as pd
 import shap
@@ -47,6 +46,20 @@ def generate_shap_values(shap_explainer, X:pd.DataFrame):
     shap_values = shap_explainer(X)
     shap_values = shap_values[:, :, 0]
     return shap_values
+
+
+def convert_shap_values_to_pd(shap_values, feature_names):
+    """
+    Converts SHAP values to a pandas DataFrame.
+
+    Args:
+        shap_values (np.array): The SHAP values to convert.
+        feature_names (List[str]): The feature names from the original data to name the DataFrame columns.
+
+    Returns:
+        DataFrame: The SHAP values as a DataFrame.
+    """
+    return pd.DataFrame(shap_values.values, columns=feature_names)
 
 
 def plot_shap_summary(shap_values, X:pd.DataFrame, num:int = None):
